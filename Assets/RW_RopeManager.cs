f5a2e5a51a4215b787dc9ecc478c74b5a0263f06
SAE1\1029260 <1029260@student.sae.edu.au>
2024-02-12 15:50:46 +1000

    starting on constraints, not quite there yet

--- Assets/RW_RopeManager.cs
+++ Assets/RW_RopeManager.cs
@@ -4,7 +4,54 @@ using UnityEngine;
 
 public class RW_RopeManager : MonoBehaviour
 {
+    public int numOfNodes = 6;
+
+    public Node[] nodes;
+    public Constraint[] constraints;
+
+    private void Start()
+    {
+        nodes = new Node[numOfNodes];
+
+        for (int i = 0; i < numOfNodes; i++)
+        {
+            nodes[i] = new Node();
+        }
 
 
+    }
+
+    public void Simulate()
+    {
+        foreach (Constraint constraint in constraints)
+        {
+            Vector2 deltaP = constraint.node2.verlet.state.pos - constraint.node1.verlet.state.pos;
+            float deltaLength = deltaP.sqrMagnitude;
+            if (deltaLength > 0)
+            {
+                float diff = (deltaLength - constraint.length) / deltaLength;
+
+                constraint.node1.verlet.state.pos += deltaP * constraint.compensate1 * diff;
+                constraint.node2.verlet.state.pos -= deltaP * constraint.compensate1 * diff;
+            }
+        }
+    }
+}
+
+
+
+public class Node
+{
+    public Verlet verlet = new Verlet();
+
+    public bool isFixed; 
 
 }
+
+public class Constraint
+{
+    public Node node1, node2;
+    public float length;
+    public float compensate1, compensate2;   
+}
+
